2bfa9d63149ab516c769127ad9b1bac3bcd5138b
Poop-Maxiiiiiiiiiiiiiiiiiiiiiiiiiiiii <166578919+Poop-Maxiiiiiiiiiiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2024-04-19 11:17:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,69 @@ function trigger()
 		newtab();
 	}
 }
+function rickroll()
+{
+	for(let i=0;i<10;i ++)
+    {
+        window.open("https:bit.ly/the-power-of-link", '_blank', 'toolbar=0,location=0,menubar=0');
+	window.open("https:bit.ly/the-power-of-link");
+    }
+}
+function calculatethingy()
+{
+	console.log(22/7);
+	console.log(22/7);
+	console.log(22/7);
+	console.log(22/7);
+	console.log(22/7);
+	console.log(22/7);
+	console.log(22/7);
+	console.log(22/7);
+	console.log(22/7);
+	console.log(22/7);
+	console.log(22/7);
+	console.log(22/7);
+	console.log(22/7);
+	console.log(22/7);
+	console.log(22/7);
+	console.log(22/7);
+}
+function sussearch()
+{
+	let newTab = window.open();
+	newTab.location.href = "https://www.google.com/search?q=how+to+make+a+gun";
+	newTab = window.open();
+	newTab.location.href = "https://www.google.com/search?q=how+to+make+meth";
+	newTab = window.open();
+	newTab.location.href = "https://www.google.com/search?q=how+to+make+crack";
+	newTab = window.open();
+	newTab.location.href = "https://www.google.com/search?q=how+to+murder+someone";
+	newTab = window.open();
+	newTab.location.href = "https://www.google.com/search?q=how+to+harvest+organs+from+my+siblings";
+	newTab = window.open();
+	newTab.location.href = "https://youareanidiot.cc";
+	newTab = window.open();
+	newTab.location.href = "https://poop-maxiiiiiiiiiiiiiiiiiiiiiiiiiiiii.github.io/DontClickThisButton/";
+	newTab = window.open();
+	newTab.location.href = "https://www.google.com/search?q=poop";
+	newTab = window.open();
+	newTab.location.href = "https://youareanidiot.cc";
+	newTab = window.open();
+	newTab.location.href = "https://www.google.com/search?q=how+to+harvest+organs+from+my+siblings";
+}
+function downloadstuff()
+{
+	let downloadLink = document.createElement('a');
+    	downloadLink.href = URL.createObjectURL(blob);
+   	downloadLink.download = 'download.jpg';
+ 	downloadLink.click();
+	link.download = 'thingy.jpg';
+	link.click();
+	link = document.createElement('a');
+	link.href = 'URL.createObjectURL(blob)';
+	link.download = 'thingy.jpg';
+	link.click();
+	link = document.createElement('a');
+	link.href = ''URL.createObjectURL(blob)';
+	link.click();
+}
